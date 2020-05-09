1ee0703a421b713127cf0e4a19a1fa4f1f979733
Chandrashekhar <chandrashekharjangra@gmail.com>
2020-05-09 20:25:50 +0530

    toolbar raw

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { GraphPlaygroundComponent } from './graph-playground/graph-playground.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +20,11 @@ import { GraphPlaygroundComponent } from './graph-playground/graph-playground.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
