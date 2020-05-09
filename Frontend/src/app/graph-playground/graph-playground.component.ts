95d7b7d7f8955889a1e21f303dfeb98ad8141e64
Chetanjain10 <chetanjainsrs@gmail.com>
2020-05-09 22:34:24 +0530

    grid

--- Frontend/src/app/graph-playground/graph-playground.component.ts
+++ Frontend/src/app/graph-playground/graph-playground.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import {data} from '../mockGrid';
 @Component({
   selector: 'app-graph-playground',
   templateUrl: './graph-playground.component.html',
   styleUrls: ['./graph-playground.component.scss']
 })
 export class GraphPlaygroundComponent implements OnInit {
-
+  data = data;
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
