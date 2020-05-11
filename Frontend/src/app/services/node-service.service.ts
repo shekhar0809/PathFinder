db9a282e5ea28f23891cf07bf742d924bee45722
Chandrashekhar <chandrashekharjangra@gmail.com>
2020-05-11 17:39:43 +0530

    unique start and end nodes

--- Frontend/src/app/services/node-service.service.ts
+++ Frontend/src/app/services/node-service.service.ts
@@ -7,6 +7,9 @@ import { node } from '../mockGrid';
 export class NodeServiceService {
   constructor() {}
 
+  startNode: node[] = [];
+  endNode: node[] = [];
+
   GetNode(e: node) {
     console.log(e);
     const coordinates = [e.row, e.col];
@@ -17,10 +20,26 @@ export class NodeServiceService {
   }
 
   start(e: node) {
+    for (let i = 0; i < this.startNode.length; i++) {
+      let item = this.startNode[i];
+      item.isStart = false;
+    }
     e.isStart ? (e.isStart = false) : (e.isStart = true);
+
+    if (e.isStart === true) {
+      this.startNode.push(e);
+    }
   }
 
   end(e: node) {
+    for (let i = 0; i < this.endNode.length; i++) {
+      let item = this.endNode[i];
+      item.isEnd = false;
+    }
     e.isEnd ? (e.isEnd = false) : (e.isEnd = true);
+
+    if (e.isEnd === true) {
+      this.endNode.push(e);
+    }
   }
 }
